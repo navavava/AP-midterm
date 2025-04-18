ac2e63e96a112f9f733113f6e3991d6639e98e89
Nava <navajay2018@gmail.com>
2025-04-18 12:20:12 +0330

    basic sketch of PremiumBehavior class

--- src/Spotify/PremiumBehavior.java
+++ src/Spotify/PremiumBehavior.java
@@ -1,4 +1,20 @@
 package Spotify;
 
-public class PremiumBehavior {
+public class PremiumBehavior implements UserBehavior{
+
+    int month;
+    @Override
+    public void createPlaylist(String Title, User Owner) {
+
+    }
+
+    @Override
+    public void playMusic(Music music) {
+
+    }
+
+    @Override
+    public void buyPremium(User owner, int month) {
+
+    }
 }
